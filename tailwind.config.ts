a8bfde05611cc559621855ad516091c1a6fcca20
reimond2207@gmail.com <remon2207>
2023-09-10 12:08:07 +0900

    chore: jitモードへ変更

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  mode: 'jit',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   plugins: [],
-};
-export default config;
+}
+export default config
