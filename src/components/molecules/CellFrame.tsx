9cbd9b2cdf64f311b6a9c1d93fab23f2f5882fdf
remon2207 <reimond2207@gmail.com>
2023-09-18 01:05:40 +0900

    fix: 属性の追加

--- src/components/molecules/CellFrame.tsx
+++ src/components/molecules/CellFrame.tsx
@@ -1,23 +1,37 @@
+'use client'
+
+import Link from 'next/link'
+
+import { Input } from '@/components/atoms/Input'
+
 type Props = {
   service: string
   email: string
   name: string
   password: string
   twoFactor: string
-  secret: string
+  note: string
 }
 
-export const CellFrame: React.FC<Props> = ({ service, email, name, password, twoFactor, secret }) => {
+export const CellFrame: React.FC<Props> = ({ service, email, name, password, twoFactor, note }) => {
+  const handleFocusAllSelect = (e: React.FocusEvent<HTMLInputElement>) => e.target.select()
   return (
     <tbody>
       <tr className="hover">
-        {/* <Cell name={name} /> */}
-        <td>{service}</td>
-        <td>{email}</td>
-        <td>{name}</td>
-        <td>{password}</td>
+        <td>
+          <Link className="break-all" href="/edit">
+            {service}
+          </Link>
+        </td>
+        <td className="break-all">{email}</td>
+        <td className="break-all">{name}</td>
+        <td>
+          <Input className="cell-input" onFocus={handleFocusAllSelect} readOnly size={5} type="text" value={password} />
+        </td>
         <td>{twoFactor}</td>
-        <td>{secret}</td>
+        <td>
+          <Input className="cell-input" onFocus={handleFocusAllSelect} readOnly size={5} type="text" value={note} />
+        </td>
       </tr>
     </tbody>
   )
