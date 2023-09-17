ca467926eea57995df607786ed3383e822d8cbab
remon2207 <reimond2207@gmail.com>
2023-09-18 01:01:45 +0900

    feat: デフォルトフォントを変更

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,15 @@
+import { Noto_Sans_JP } from 'next/font/google'
+
 import '@/styles/globals.css'
+
+const notoSansJP = Noto_Sans_JP({
+  subsets: ['latin'],
+})
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <html lang="ja">
-      <body className="h-screen">{children}</body>
+    <html className={notoSansJP.className} lang="ja">
+      <body className="h-screen">
     </html>
   )
 }
