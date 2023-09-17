c64f7dc01220479acf4ff1429399d8a45f44af89
remon2207 <reimond2207@gmail.com>
2023-09-18 01:01:58 +0900

    feat: ヘッダーを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { Noto_Sans_JP } from 'next/font/google'
 
+import { Header } from '@/components/organisms/Header'
+
 import '@/styles/globals.css'
 
 const notoSansJP = Noto_Sans_JP({
@@ -10,6 +12,9 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html className={notoSansJP.className} lang="ja">
       <body className="h-screen">
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
