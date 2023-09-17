d827f21f976e49feec2f5ab9a64ec489245b7e0a
remon2207 <reimond2207@gmail.com>
2023-09-18 01:09:05 +0900

    feat: パスワード情報のスキーマを追加

---
+++ src/utils/YupSchema.ts
@@ -0,0 +1,10 @@
+import * as yup from 'yup'
+
+export const schema = yup.object({
+  service: yup.string().required('サービス名を入力してください'),
+  email: yup.string().email('有効なメールアドレスを入力してください').required('メールアドレスを入力してください'),
+  name: yup.string().required('名前を入力してください'),
+  password: yup.string().required('パスワードを入力してください'),
+  twoFactor: yup.boolean().required(),
+  note: yup.string().required(),
+})
