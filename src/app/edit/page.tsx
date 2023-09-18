39d58dbfbbe4b09bffecbac8b3b082b59dcd6808
remon2207 <reimond2207@gmail.com>
2023-09-19 01:27:24 +0900

    feat: Formコンポーネントを追加

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+
+import { Form } from '@/components/organisms/Form'
+
 import type { NextPage } from 'next'
 
 const Edit: NextPage = () => {
-  return <>edit</>
+  return (
+    <>
+      <Form />
+    </>
+  )
 }
 
 export default Edit
