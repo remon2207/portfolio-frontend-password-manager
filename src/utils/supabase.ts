2ee0c923a5f63aeccde280f5305b0c6ef5c361b2
remon2207 <reimond2207@gmail.com>
2023-09-20 03:19:35 +0900

    refactor: import typeを追加

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-import { Database } from '@/types/schema'
+import type { Database } from '@/types/schema'
 
 export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
