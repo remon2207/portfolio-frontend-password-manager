160ca53fb444a0bb6b10744d4e3c2968ab52c2cb
remon2207 <reimond2207@gmail.com>
2023-09-20 02:25:29 +0900

    feat: キャッシュの設定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,8 @@ const notoSansJP = Noto_Sans_JP({
   subsets: ['latin'],
 })
 
+export const revalidate = 0
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html className={notoSansJP.className} lang="ja">
