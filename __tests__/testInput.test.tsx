5f394918ce06a5eea0a6fd511f363573e51d3f6f
remon2207 <reimond2207@gmail.com>
2023-09-20 06:00:32 +0900

    test

--- __tests__/testInput.test.tsx
+++ __tests__/testInput.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { Form } from '@/components/organisms/Form'
 
@@ -23,6 +24,32 @@ describe('InputForm component', () => {
 
     elems.forEach((elem) => {
       expect(elem).toBeInTheDocument()
+    })
+  })
+
+  test('Enter the form and click the button', async () => {
+    render(
+      <Form
+        emailDefaultValue="email@email.com"
+        nameDefaultValue="name"
+        passwordDefaultValue="password"
+        serviceDefaultValue="service"
+      />
+    )
+
+    const submitButton = screen.getByRole('button', { name: '送信' })
+
+    await userEvent.click(submitButton)
+
+    const serviceErr = screen.queryByTestId('serviceError')
+    const emailErr = screen.queryByTestId('emailError')
+    const nameErr = screen.queryByTestId('nameError')
+    const passwordErr = screen.queryByTestId('passwordError')
+
+    const errors = [serviceErr, emailErr, nameErr, passwordErr]
+
+    errors.forEach((error) => {
+      expect(error).not.toBeInTheDocument()
     })
   })
 })
