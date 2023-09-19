f1ac1c3a8fa94536bf65d627bb3128712a923ec6
remon2207 <reimond2207@gmail.com>
2023-09-20 05:25:53 +0900

    test: テスト削除

--- __tests__/testInput.test.tsx
+++ __tests__/testInput.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 
 import { Form } from '@/components/organisms/Form'
 
@@ -24,26 +23,6 @@ describe('InputForm component', () => {
 
     elems.forEach((elem) => {
       expect(elem).toBeInTheDocument()
-    })
-  })
-
-  test('Enter the email and click the button', async () => {
-    render(<Form emailDefaultValue="email" />)
-
-    const submitButton = screen.getByRole('button', { name: '送信' })
-
-    await userEvent.click(submitButton)
-
-    // const serviceErr = screen.queryByTestId('serviceError')
-    const emailErr = screen.queryByTestId('emailError')
-    // const nameErr = screen.queryByTestId('nameError')
-    // const passwordErr = screen.queryByTestId('passwordError')
-
-    // const errors = [serviceErr, emailErr, nameErr, passwordErr]
-    const errors = [emailErr]
-
-    errors.forEach((error) => {
-      expect(error).toBeInTheDocument()
     })
   })
 })
