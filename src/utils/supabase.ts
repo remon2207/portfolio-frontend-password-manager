ce3ed78573d004d9c6082308a8aeec9a016021b6
remon2207 <reimond2207@gmail.com>
2023-09-19 21:24:01 +0900

    refactor: 型を付与

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
+import { Database } from '@/types/schema'
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
   auth: { persistSession: false },
 })
