0269fe3e554d12f2b0ce2f67d4029d41c1d7532e
remon2207 <reimond2207@gmail.com>
2023-09-20 03:55:39 +0900

    test

--- __tests__/testInput.test.tsx
+++ __tests__/testInput.test.tsx
@@ -1,55 +1,55 @@
-// import { render, screen } from '@testing-library/react'
-// import userEvent from '@testing-library/user-event'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
-// import { Form } from '@/components/organisms/Form'
+import { Form } from '@/components/organisms/Form'
 
-// jest.mock('next/navigation', () => ({
-//   useRouter: () => ({
-//     push: jest.fn(),
-//   }),
-//   usePathname: jest.fn(),
-// }))
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({
+    push: jest.fn(),
+  }),
+  // usePathname: jest.fn(),
+}))
 
-// describe('InputForm component', () => {
-//   test('Text box exist', () => {
-//     render(<Form emailDefaultValue="" nameDefaultValue="" passwordDefaultValue="" serviceDefaultValue="" />)
+describe('InputForm component', () => {
+  test('Text box exist', () => {
+    render(<Form emailDefaultValue="" nameDefaultValue="" passwordDefaultValue="" serviceDefaultValue="" />)
 
-//     const service = screen.getByTestId('service')
-//     const email = screen.getByTestId('email')
-//     const name = screen.getByTestId('name')
-//     const password = screen.getByTestId('password')
-//     const submitButton = screen.getByRole('button', { name: '送信' })
+    const service = screen.getByTestId('service')
+    const email = screen.getByTestId('email')
+    const name = screen.getByTestId('name')
+    const password = screen.getByTestId('password')
+    const submitButton = screen.getByRole('button', { name: '送信' })
 
-//     const elems = [service, email, name, password, submitButton]
+    const elems = [service, email, name, password, submitButton]
 
-//     elems.forEach((elem) => {
-//       expect(elem).toBeInTheDocument()
-//     })
-//   })
+    elems.forEach((elem) => {
+      expect(elem).toBeInTheDocument()
+    })
+  })
 
-//   test('Enter the form and click the button', async () => {
-//     render(
-//       <Form
-//         emailDefaultValue="email@email.com"
-//         nameDefaultValue="name"
-//         passwordDefaultValue="password"
-//         serviceDefaultValue="service"
-//       />
-//     )
+  test('Enter the form and click the button', async () => {
+    render(
+      <Form
+        emailDefaultValue="email@email.com"
+        nameDefaultValue="name"
+        passwordDefaultValue="password"
+        serviceDefaultValue="service"
+      />
+    )
 
-//     const submitButton = screen.getByRole('button', { name: '送信' })
+    const submitButton = screen.getByRole('button', { name: '送信' })
 
-//     await userEvent.click(submitButton)
+    await userEvent.click(submitButton)
 
-//     const serviceErr = screen.queryByTestId('serviceError')
-//     const emailErr = screen.queryByTestId('emailError')
-//     const nameErr = screen.queryByTestId('nameError')
-//     const passwordErr = screen.queryByTestId('passwordError')
+    const serviceErr = screen.queryByTestId('serviceError')
+    const emailErr = screen.queryByTestId('emailError')
+    const nameErr = screen.queryByTestId('nameError')
+    const passwordErr = screen.queryByTestId('passwordError')
 
-//     const errors = [serviceErr, emailErr, nameErr, passwordErr]
+    const errors = [serviceErr, emailErr, nameErr, passwordErr]
 
-//     errors.forEach((error) => {
-//       expect(error).not.toBeInTheDocument()
-//     })
-//   })
-// })
+    errors.forEach((error) => {
+      expect(error).not.toBeInTheDocument()
+    })
+  })
+})
