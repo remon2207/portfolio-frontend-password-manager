9d870abc20fbd6b747ca634ac2f2194c1b5ef385
remon2207 <reimond2207@gmail.com>
2023-09-20 13:10:26 +0900

    test error

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -2,6 +2,10 @@ import { createClient } from '@supabase/supabase-js'
 
 import type { Database } from '@/types/schema'
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
-  auth: { persistSession: false },
-})
+export const supabase = createClient<Database>(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  {
+    auth: { persistSession: false },
+  }
+)
