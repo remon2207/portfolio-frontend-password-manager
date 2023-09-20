5739e23a703224c0999b2ff8b3c672cd06a5e213
remon2207 <reimond2207@gmail.com>
2023-09-20 12:42:18 +0900

    test error

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 import type { Database } from '@/types/schema'
 
 export const supabase = createClient<Database>(
-  'https://fduekqwmswnygftcwzru.supabase.co',
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
   {
     auth: { persistSession: false },
