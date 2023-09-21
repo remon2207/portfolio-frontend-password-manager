ec1dca484846ca80c28aea822bde91481a0af822
remon2207 <reimond2207@gmail.com>
2023-09-22 05:32:05 +0900

    test: mod

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 import type { Database } from '@/types/schema'
 
+process.env.NEXT_PUBLIC_SUPABASE_URL = 'https://fduekqwmswnygftcwzru.supabase.co'
+
 export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
