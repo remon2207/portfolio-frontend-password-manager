555c1686a59e60cb65290173fdd42318e47017d4
remon2207 <reimond2207@gmail.com>
2023-09-22 05:28:16 +0900

    fix: supabaseUrlを削除

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,6 +4,6 @@ import type { Database } from '@/types/schema'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+export const supabase = createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL, supabaseKey, {
   auth: { persistSession: false },
 })
