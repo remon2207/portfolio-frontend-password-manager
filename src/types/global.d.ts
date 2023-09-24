3715dc0eacb33a5ec9b7e54b32e3fb94389a56fa
reimond2207@gmail.com <remon2207>
2023-09-24 23:22:44 +0900

    fix: secretとの追加とエラーを修正

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,6 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_SUPABASE_URL: string
     NEXT_PUBLIC_SUPABASE_ANON_KEY: string
+    NEXTAUTH_SECRET: string
     GITHUB_ID: string
     GITHUB_SECRET: string
   }
